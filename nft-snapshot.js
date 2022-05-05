5f0a8f476071cd671ca3fb037906fcf14708df44
Etheria-chan <user.email=85286825+etheria-chan@users.noreply.github.com>
2022-05-05 14:11:06 -0400

    Updated to work properly with OpenSea

--- nft-snapshot.js
+++ nft-snapshot.js
@@ -13,78 +13,32 @@ const CHAIN_ID = +process.env.CHAIN_ID;
 const OPENSEA_TOKEN = process.env.OPENSEA_TOKEN;
 let openseaQueried = false;
 
-async function ownerByTokenId(contract, startId, endId) {
-    const ownerByTokenIdWriter = createCsvWriter({
-        path: 'ownerByTokenId.csv',
-        header: [
-          {id: 'id', title: 'ID'},
-          {id: 'address', title: 'Address'},
-        ]
-    });
-
-    for (let i = startId; i <= endId; i++) {
-        try {
-            if (i % 100 == 0) console.log(`Checkpoint: ${i}`)
-            const owner = await contract.ownerOf(i);            
-            dbData = [{ id: i, address: owner }];
-            await ownerByTokenIdWriter.writeRecords(dbData)
-        } catch (err) {
-            console.log(`Token ${i} Error:`)
-            console.log(err)
-            dbData = [{ id: i, address: 'ERROR' }];
-            await ownerByTokenIdWriter.writeRecords(dbData)
-        }
-    }
-}
-
-async function tokensByOwner(contract, startId, endId) {
-    const tokensByOwnerWriter = createCsvWriter({
-        path: 'tokensByOwner.csv',
-        header: [
-            {id: 'address', title: 'Address'},
-            {id: 'count', title: 'Count'},
-            {id: 'ids', title: 'IDs'},
-        ]
-    });
-
-    console.log(contract);
-
-    const idsByOwner = {}
-    for (let i = startId; i <= endId; i++) {
-        try {
-            if (i % 100 == 0) console.log(`Checkpoint: ${i}`)
-            const owner = await contract.ownerOf(i);
-            owner in idsByOwner ? idsByOwner[owner].push(i) : idsByOwner[owner] = [i]
-        } catch (err) {
-            'Errors' in idsByOwner ? idsByOwner['Errors'].push(i) : idsByOwner['Errors'] = [i]
-            console.log(`Token ${i} Error:`)
-            console.log(err)
-        }
-    }
-    const outputData = Object.keys(idsByOwner).map(address => ({ address, count: idsByOwner[address].length, ids: idsByOwner[address] }))
-    await tokensByOwnerWriter.writeRecords(outputData)
-}
-
 function getContract(address) {
     const provider = new ethers.providers.JsonRpcProvider(PROVIDER_ENDPOINT, CHAIN_ID);
     const abi = ["function ownerOf (uint256) view returns (address)"];
     return new ethers.Contract(address, abi, provider);
 }
 
-async function main(format, contract, startId, endId) {
-    const timeBefore = new Date().getTime();
-    
-    if (format == "tokensByOwner") {
-        await tokensByOwner(contract, startId, endId)
-    } else if (format == "ownerByTokenId") {
-        await ownerByTokenId(contract, startId, endId)
-    } else {
-        console.log("Invalid format. Please use 'tokensByOwner' or 'ownerByToken'.")
+async function getErcAssets(contract, startId, endId) {
+    const assets = [];
+
+    for (let id = startId; id <= endId; id++) {
+        try {
+            if (id % 100 == 0) console.log(`Checkpoint: ${id}`)
+            assets.push( {
+                id,
+                owner: await contract.ownerOf(id)
+            });
+        } catch (err) {
+            console.log(`Token ${id} Error:`, err);
+            assets.push( {
+                id,
+                owner: 'ERROR'
+            });
+        }
     }
-    
-    const timeAfter = new Date().getTime();
-    const timeTaken = (timeAfter - timeBefore) / 1000;
-    console.log(`Script Completed. Total Run Time: ${timeTaken} Seconds`)
+
+    return assets; 
 }
 
 async function getOpenseaAssets(slug, array = undefined, next = undefined) {
@@ -99,6 +53,7 @@ async function getOpenseaAssets(slug, array = undefined, next = undefined) {
     }
 
     const url = `https://api.opensea.io/api/v1/assets?collection_slug=${slug}&limit=50${next ? `&next=${next}` : ''}`;
+    console.log(url);
     const headers = {
         'X-API-KEY': OPENSEA_TOKEN
     };
@@ -108,7 +63,11 @@ async function getOpenseaAssets(slug, array = undefined, next = undefined) {
     }
 
     const assetsBody = await response.json();
-    array.push(...assetsBody.assets);
+    console.log(`Received ${assetsBody.assets.length} assets`);
+    array.push(...assetsBody.assets.map(asset => ({
+        id: asset.id,
+        owner: asset.owner.address,
+    })));
     
     if (assetsBody.next) {
         return getOpenseaAssets(slug, array, assetsBody.next);
@@ -117,20 +76,67 @@ async function getOpenseaAssets(slug, array = undefined, next = undefined) {
     }
 }
 
+async function writeOwnerByTokenId(assets) {
+    const ownerByTokenIdWriter = createCsvWriter({
+        path: 'ownerByTokenId.csv',
+        header: [
+            {id: 'id', title: 'ID'},
+            {id: 'owner', title: 'Owner'},
+        ]
+    });
+
+    try {
+        await ownerByTokenIdWriter.writeRecords(assets);
+    } catch (err) {
+        console.log('Failed writing CSV:', err);
+    }
+}
+
+async function writeTokensByOwner(assets) {
+    const tokensByOwnerWriter = createCsvWriter({
+        path: 'tokensByOwner.csv',
+        header: [
+            {id: 'address', title: 'Address'},
+            {id: 'count', title: 'Count'},
+            {id: 'ids', title: 'IDs'},
+        ]
+    });
+
+    const idsByOwner = {}
+
+    for (const asset of assets) {
+        asset.owner in idsByOwner ? idsByOwner[asset.owner].push(asset.id) : idsByOwner[asset.owner] = [ asset.id ];
+    }
+
+    const outputData = Object.keys(idsByOwner).map(address => ({ address, count: idsByOwner[address].length, ids: idsByOwner[address] }))
+    await tokensByOwnerWriter.writeRecords(outputData)
+}
+
+async function timeAndExecute(callback) {
+    const timeBefore = new Date().getTime();
+    await callback();   
+    const timeAfter = new Date().getTime();
+    const timeTaken = (timeAfter - timeBefore) / 1000;
+    console.log(`Script Completed. Total Run Time: ${timeTaken} Seconds`)
+}
+
+async function writeAssets(assets, format) {
+    timeAndExecute(async () => {
+        switch (format) {
+            case "tokensByOwner":
+                await writeTokensByOwner(assets);
+                break;
+            case "ownerByTokenId":
+                await writeOwnerByTokenId(assets);
+                break;
+            default:
+                console.log("Invalid format. Please use 'tokensByOwner' or 'ownerByToken'.")
+        }
+    });
+}
+
 yargs(hideBin(process.argv))
 .option({
-    ['startIndex']: {
-        alias: 's',
-        desc: 'start index (numeric) for the collection',
-        type: 'number',
-        required: true,
-    },
-    ['endIndex']: {
-        alias: 'e',
-        desc: 'end index (numeric) for the collection',
-        type: 'number',
-        required: true,
-    },
     ['format']: {
         alias: 'f',
         desc: 'format for the results (tokensByOwner or idsByOwner)',
@@ -147,11 +153,24 @@ yargs(hideBin(process.argv))
             desc: 'the contract address for the collection',
             type: 'string',
             required: true,
-        }
+        },
+        ['startIndex']: {
+            alias: 's',
+            desc: 'start index (numeric) for the collection',
+            type: 'number',
+            required: true,
+        },
+        ['endIndex']: {
+            alias: 'e',
+            desc: 'end index (numeric) for the collection',
+            type: 'number',
+            required: true,
+        },
     },
-    (args) => {
+    async (args) => {
         const contract = getContract(args.address);
-        main(args.format, contract, +args.startIndex, +args.endIndex);
+        const assets = await getErcAssets(contract, +args.startIndex, +args.endIndex);
+        await writeAssets(assets, args.format);
     }
 )
 .command(
@@ -166,8 +185,12 @@ yargs(hideBin(process.argv))
         }
     }, 
     async (args) => {
-        const assets = await getOpenseaAssets(args.slug);
-        console.log(assets);
+        const assets = (await getOpenseaAssets(args.slug))
+        .map(asset => ({
+            id: asset.id,
+            owner: asset.owner
+        }));
+        writeAssets(assets, args.format);
     }
 )
 .argv;
